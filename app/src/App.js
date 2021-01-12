819a7fce385aa923cc1581713115de11d40cf860
unknown <tompsherman@gmail.com>
2021-01-11 21:33:38 -0500

    adjust flow

--- app/src/App.js
+++ app/src/App.js
@@ -5,59 +5,12 @@ import './App.css'
 const App = () => {
   return (
     <div className="App">
-      {/* Remove everything from this line to line 37 */}
-      <div className="image-container">
-        <img
-          alt="Lambda loves our llamas!"
-          src="https://assets-global.website-files.com/5cd091cfb5499f22bdf72905/5dcda59e63bb6ae5c9282801_small-red-logo.png"
-        ></img>
-      </div>
-      <h2>
-        Welcome <p className="welcome-bottom">Lambda student!</p>
-      </h2>
-      <h4>Your React Application is Ready</h4>
-      <h4>Now go work your magic!</h4>
-      <p className="filler-text">This is filler, no need to keep it!</p>
-      <div className="footer">
-        <a
-          href="https://lambdaschool.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Visit the Lambda Llama home
-        </a>
-        <br />
-        <div>
-          <p>Created using Create-React-App</p>
-          <p>Modified and maintained by Lambda School graduate Zac Smith</p>
-          <p>
-            <a
-              href="https://github.com/mrzacsmith"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github
-            </a>{" "}
-            ::{" "}
-            <a
-              href="https://www.npmjs.com/~mrzacsmith"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              NPM
-            </a>{" "}
-            ::{" "}
-            <a
-              href="https://twitter.com/mrzacsmith"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Twitter
-            </a>
-          </p>
-        </div>
-      </div>
-      {/* Remove everything from line 8 to here */}
+      <h1>American Repatriatization Organization</h1>
+      <h3>Mission/Vision:</h3>
+      <h3>Doctrine of Discovery</h3>
+      <h3>What you can do</h3>
+      <h3>Zip Code/Tribe lookup</h3>
+      <h4>with people of note to contact</h4>
     </div>
   );
 };
