0374d78b6c2f5705d395c5102f4e6b86704074be
unknown <tompsherman@gmail.com>
2021-01-12 08:26:52 -0500

    content scaffolding

--- app/src/App.js
+++ app/src/App.js
@@ -7,10 +7,20 @@ const App = () => {
     <div className="App">
       <h1>American Repatriatization Organization</h1>
       <h3>Mission/Vision:</h3>
+      <p> - mission </p>
+      <p> - vision </p>
       <h3>Doctrine of Discovery</h3>
+      <p> - Columbus & the Pope </p>
+      <p> - - pre-Enlightenment</p>
       <h3>What you can do</h3>
+      <p> - how to make things right</p>
       <h3>Zip Code/Tribe lookup</h3>
-      <h4>with people of note to contact</h4>
+      <p> - input form => zip code </p>
+      <p> - returns map of tribal area</p>
+      <p> - history / facts of tribe</p>
+      <h4>with people of note to contact:</h4>
+      <p> - leaders </p>
+      <p> - lawyers </p>
     </div>
   );
 };
